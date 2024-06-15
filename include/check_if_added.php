92f81f95f8cd22bce24224777805b2ace36e0d91
rohit-kumar-india <rohit.rk2146@gmail.com>
2024-06-15 23:21:00 +0530

    Added config variables for database connection in common.php

--- include/check_if_added.php
+++ include/check_if_added.php
@@ -1,6 +1,9 @@
 <?php
+require 'include/common.php';
+
 function check_if_added_to_cart($item_id){
-    $con= mysqli_connect('localhost', 'root', 'root', 'store') or die(mysqli_error($con));
+    global $con;
+    // $con= mysqli_connect($host, 'root', '', 'e-store') or die(mysqli_error($con));
     $user_id=$_SESSION['user_id'];
     $check_cart_query="select * from users_items where item_id='$item_id' and user_id ='$user_id' and status='Added to cart'";
     $check_cart_submit= mysqli_query($con, $check_cart_query) or die(mysqli_error($con));
--- include/common.php
+++ include/common.php
@@ -1,4 +1,10 @@
 <?php
-$con= mysqli_connect('localhost', 'root', 'root', 'store') or die(mysqli_error($con));
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'lifestyle_store';
+
+$con= mysqli_connect($host, $username, $password, $database) or die(mysqli_error($con));
 session_start();
 ?>
